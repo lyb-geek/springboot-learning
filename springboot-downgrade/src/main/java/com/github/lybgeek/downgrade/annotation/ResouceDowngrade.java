1e458188eb338958d20313d88d5166b677d9b0d0
linyb1 <410480180@qq.com>
2019-11-22 14:48:12 +0800

    springboot方法级别动态限流降级例子

--- springboot-downgrade/src/main/java/com/github/lybgeek/downgrade/annotation/ResouceDowngrade.java
+++ springboot-downgrade/src/main/java/com/github/lybgeek/downgrade/annotation/ResouceDowngrade.java
@@ -11,12 +11,28 @@ import java.lang.annotation.Target;
 @Documented
 public @interface ResouceDowngrade {
 
-  int maxThreshold() default 3;
+  /**
+   * 最大阈值，为了演示方便，设置成0
+   * @return
+   */
+  int maxThreshold() default 0;
 
+  /**
+   * 限流回调实现类
+   * @return
+   */
   Class fallbackClass();
 
+  /**
+   * 限流回调方法名称，不填默认为被限流的方法名称
+   * @return
+   */
   String fallbackMethod() default "";
 
+  /**
+   * 方法资源ID，一个方法代表一个资源ID，这个id必须是唯一不可重复的值
+   * @return
+   */
   String resouceId();
 
 }
