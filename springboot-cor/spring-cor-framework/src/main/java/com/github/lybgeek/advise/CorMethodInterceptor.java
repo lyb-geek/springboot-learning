7a3a0f5866e5a6e227f65565fab96cb1d33a65c0
lyb-geek <410480180@qq.com>
2022-04-02 11:40:49 +0800

    Update CorMethodInterceptor.java

--- springboot-cor/spring-cor-framework/src/main/java/com/github/lybgeek/advise/CorMethodInterceptor.java
+++ springboot-cor/spring-cor-framework/src/main/java/com/github/lybgeek/advise/CorMethodInterceptor.java
@@ -1,26 +1,56 @@
-package com.github.lybgeek.advise;
+package com.github.lybgeek.cor;
 
 
-import com.github.lybgeek.cor.CorHandlerInterceptor;
+import com.github.lybgeek.cor.chain.MethodInterceptorChain;
+import com.github.lybgeek.cor.handler.AbstarctHandler;
 import com.github.lybgeek.cor.model.Invocation;
-import org.aopalliance.intercept.MethodInterceptor;
-import org.aopalliance.intercept.MethodInvocation;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.util.CollectionUtils;
 
-public class CorMethodInterceptor implements MethodInterceptor {
+import java.util.List;
 
-    private CorHandlerInterceptor corHandlerInterceptor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CorHandlerInterceptor {
 
-    public CorMethodInterceptor(CorHandlerInterceptor corHandlerInterceptor) {
-        this.corHandlerInterceptor = corHandlerInterceptor;
+    private MethodInterceptorChain chain;
+
+
+    public Object invoke(Invocation invocation) throws Exception {
+        List<AbstarctHandler> abstarctHandlers = chain.getHanlders();
+        if(CollectionUtils.isEmpty(abstarctHandlers)){
+            invocation.invoke();
+        }
+
+        boolean isCanExec = true;
+        int canExecCount = 0;
+        for (AbstarctHandler abstarctHandler : abstarctHandlers) {
+             if(!abstarctHandler.preHandler(invocation)){
+                 isCanExec = false;
+                 break;
+             }
+            canExecCount++;
+        }
+
+           try{
+               if(isCanExec){
+                   return invocation.invoke();
+               }
+           }catch (Exception e){
+               throw new Exception(e);
+           }finally {
+               for (int i = 0; i < canExecCount; i++) {
+                   int j = canExecCount - i - 1;
+                   abstarctHandlers.get(j).afterCompletion(invocation);
+               }
+           }
+
+        return null;
     }
 
-    @Override
-    public Object invoke(MethodInvocation invocation) throws Throwable {
-        Invocation invoker = Invocation.builder()
-                .args(invocation.getArguments())
-                .method(invocation.getMethod())
-                .target(invocation.getThis()).build();
 
-        return corHandlerInterceptor.invoke(invoker);
-    }
+
 }
